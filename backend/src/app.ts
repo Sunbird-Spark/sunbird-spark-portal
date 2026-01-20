16d27209f1f9e6d3d652298bb32ed542a39b192f
JeraldJF <jeraldj@sanketika.in>
2026-01-20 18:16:08 +0530

    #SS-I41: removed limits

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,8 +10,8 @@ import { kongProxy } from './proxies/kongProxy.js';
 export const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.use(session({
     store: sessionStore,
