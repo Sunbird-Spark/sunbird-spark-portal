2f577212d3d19b7b387312a8ee08e1581ddf81a1
JeraldJF <jeraldj@sanketika.in>
2026-01-21 10:53:59 +0530

    #SS-I41: lint fix

--- backend/src/proxies/kongProxy.ts
+++ backend/src/proxies/kongProxy.ts
@@ -4,7 +4,6 @@ import * as http from 'http';
 import { decorateRequestHeaders } from '../utils/proxyUtils.js';
 import logger from '../utils/logger.js';
 import { envConfig } from '../config/env.js';
-import _ from 'lodash';
 
 const KONG_URL = envConfig.KONG_URL;
 
