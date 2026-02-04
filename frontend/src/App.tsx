fd5f98020e022cc2b72442eba108b03f44350df3
Deeksha Deepak <deekshad1502@gmail.com>
2026-02-04 17:29:35 +0530

    chore: remove unused AppCoreService import.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
-import AppCoreService from './services/AppCoreService';
 export default function App() {
   return (
     <BrowserRouter>
