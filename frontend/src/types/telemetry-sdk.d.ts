1cb3a13e80ffcb5aa41998a876ea2fae59af9223
Deeksha Deepak <deekshad1502@gmail.com>
2026-02-04 15:43:01 +0530

    refactor: Update telemetry SDK to be loaded as a global script and simplify its type declaration.

--- frontend/src/types/telemetry-sdk.d.ts
+++ frontend/src/types/telemetry-sdk.d.ts
@@ -1,5 +1,1 @@
-declare module '@project-sunbird/telemetry-sdk' {
-    export function getFingerPrint(
-        callback: (deviceId: string, components: any[], version: string) => void
-    ): void;
-}
+declare module '@project-sunbird/telemetry-sdk';
