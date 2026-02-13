a81494cc5ed66cce1c5ef0eed5b7d1dede58ff26
JeraldJF <jeraldj@sanketika.in>
2026-02-13 17:41:44 +0530

    #SS-I59: fix: route fix for app info route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,7 +28,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.get('/health', checkHealth);
-app.get('/app/v1/info', getAppInfo);
+app.get('/portal/app/v1/info', getAppInfo);
 
 // Portal Authentication Routes (Login, Callback, Logout)
 app.use('/portal', portalAuthRoutes);
