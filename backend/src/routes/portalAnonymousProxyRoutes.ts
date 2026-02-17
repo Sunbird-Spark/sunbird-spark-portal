3dc2601a045442f0914d747a81618efcddbb596b
Srivatsa K <165643502+Srivatsak2002@users.noreply.github.com>
2026-02-17 10:43:00 +0530

    feat: Add new route for course hierarchy in portalAnonymousProxyRoutes to enhance API functionality.

--- backend/src/routes/portalAnonymousProxyRoutes.ts
+++ backend/src/routes/portalAnonymousProxyRoutes.ts
@@ -7,6 +7,7 @@ import { validateReadAPI } from '../middlewares/formsValidator.js';
 const router = express.Router();
 
 router.get('/content/v1/read/*rest', kongProxy);
+router.get('/course/v1/hierarchy/*rest', kongProxy);
 router.post('/org/v2/search', kongProxy);
 router.get('/data/v1/system/settings/get/*rest', kongProxy);
 router.post('/composite/v1/search', kongProxy);
