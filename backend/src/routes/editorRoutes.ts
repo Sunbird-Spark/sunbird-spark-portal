4896444f6e6f08e8d876e3860e6bcab0d6be8811
JeraldJF <jeraldj@sanketika.in>
2026-02-24 12:25:50 +0530

    #SS-I67: action slugged framework api from content editor

--- backend/src/routes/editorRoutes.ts
+++ backend/src/routes/editorRoutes.ts
@@ -11,6 +11,7 @@ const editorRoutes: string[] = [
     '/collection/v1/export/*rest',
     '/collection/v1/import/*rest',
     '/data/v1/form/read',
+    '/framework/v1/read/*rest'
 ]
 
 router.all(editorRoutes, sessionMiddleware, keycloak.middleware({ admin: '/home', logout: '/portal/logout' }), keycloak.protect(), kongProxy)
