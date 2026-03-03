443d5b37e9c6065cc182ed40c01a871ffa9cb3fa
JeraldJF <jeraldj@sanketika.in>
2026-03-03 16:19:28 +0530

    #SS-I72: feat: error causing iframe renederer plugin removed

--- frontend/src/services/editors/content-editor/ContentEditorService.ts
+++ frontend/src/services/editors/content-editor/ContentEditorService.ts
@@ -95,7 +95,6 @@ export class ContentEditorService {
       previewConfig: {
         repos: ['/content-plugins/renderer'],
         plugins: [
-          { id: 'org.sunbird.iframeEvent', ver: '1.0', type: 'plugin' },
           { id: 'org.sunbird.player.endpage', ver: '1.1', type: 'plugin' },
         ],
         showStartPage: true,
